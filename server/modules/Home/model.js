06656957b2dd2765e32a1136293b4b25ef6ecd2b
tuongduy10 <vtngduy101120@gmail.com>
2022-04-02 21:56:00 +0700

    edit

--- server/modules/Home/model.js
+++ server/modules/Home/model.js
@@ -1,3 +1,4 @@
+const { Decimal128 } = require('mongodb');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -6,8 +7,23 @@ const PerfumeSchema = new Schema({
         type: String,
         required: true
     },
-    
-}) 
+    Perfume_Price:{
+        type: Decimal128,
+        required: true,
+    },
+    Perfume_Image:{
+        type: String,
+        required: true,
+    },
+    Perfume_Description:{
+        type: String,
+        required: true,
+    },
+    Type:{
+        type: Object,
+        required: true,
+    }
+}, {timestamps: true});
 
 const Perfume = mongoose.model('Perfume', PerfumeSchema);
 module.exports = Perfume;
